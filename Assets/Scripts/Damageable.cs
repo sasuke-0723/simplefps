11b4db72f441d53c9f26edc8d6397516cd55ee3c
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-12-11 01:38:36 +0900

    initial commit

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+/// <summary>
+/// ダメージとライフを制御するコンポーネント
+/// </summary>
+public class Damageable : MonoBehaviour
+{
+    /// <summary>初期ライフ</summary>
+    [SerializeField, Range(1, 99999)] int m_initialLife = 5000;
+    /// <summary>現在のライフ</summary>
+    int m_life;
+    PhotonView m_photonView;
+
+    private void Start()
+    {
+        m_life = m_initialLife;
+        m_photonView = GetComponent<PhotonView>();
+    }
+
+    /// <summary>
+    /// ダメージを与える
+    /// </summary>
+    /// <param name="playerId">ダメージを与えたプレイヤーのID</param>
+    /// <param name="damage">ダメージ量</param>
+    public void Damage(int playerId, int damage)
+    {
+        m_life -= damage;
+        Debug.LogFormat("Player {0} が Player {1} の {2} に {3} のダメージを与えた", playerId, m_photonView.Owner.ActorNumber, name, damage);
+
+        object[] parameters = new object[] { m_life };
+        m_photonView.RPC("SyncLife", RpcTarget.All, parameters);
+    }
+
+    /// <summary>
+    /// ダメージを与えたことをクライアント間で同期する
+    /// </summary>
+    /// <param name="currentLife"></param>
+    [PunRPC]
+    void SyncLife(int currentLife)
+    {
+        m_life = currentLife;
+        Debug.LogFormat("Player {0} の {1} の残りライフは {2}", m_photonView.Owner.ActorNumber, gameObject.name, m_life);
+    }
+}
---
+++ Assets/Scripts/NetworkPlayerController.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+/// <summary>
+/// プレイヤーの基本操作を処理するコンポーネント
+/// </summary>
+[RequireComponent(typeof(CharacterController), typeof(AudioSource))]
+public class NetworkPlayerController : MonoBehaviour
+{
+    /// <summary>FPS のカメラ</summary>
+    [SerializeField] Camera m_mainCamera;
+    /// <summary>照準となる UI オブジェクト</summary>
+    [SerializeField] Image m_crosshair;
+    /// <summary>照準に敵を捕らえていない時の色</summary>    
+    [SerializeField] Color m_noTarget;
+    /// <summary>照準に敵を捕らえている時の色</summary>
+    [SerializeField] Color m_onTarget;
+    /// <summary>射撃可能距離</summary>
+    [SerializeField, Range(10, 200)] float m_shootRange = 100f;
+    /// <summary>照準の Ray が当たる Layer</summary>
+    [SerializeField] LayerMask m_shootingLayer;
+
+    /// <summary>攻撃したらダメージを与えられる対象</summary>
+    Damageable m_target;
+    PhotonView m_photonView;
+    private CharacterController m_control;
+    private Transform m_cameraTransform;
+
+    private Vector3 m_moveDirection;
+    private Vector3 m_camRotation;
+
+    public float speed = 6;
+    public float jumpSpeed = 1.5f;
+    public float gravity = 0.5f;
+
+    /// <summary>下に向くことのできる最小角度</summary>
+    [Range(-45, -15)]
+    public int minAngle = -30;
+    /// <summary>上に向くことのできる最大角度</summary>
+    [Range(30, 80)]
+    public int maxAngle = 45;
+    /// <summary>マウスの感度</summary>
+    [Range(50, 500)]
+    public int sensitivity = 200;
+
+    private void Start()
+    {
+        m_control = GetComponent<CharacterController>();
+        m_photonView = GetComponent<PhotonView>();
+
+        // 自分が生成したプレイヤーのカメラだけを有効にする
+        if (m_photonView.IsMine)
+        {
+            m_mainCamera.gameObject.SetActive(true);
+            m_cameraTransform = m_mainCamera.transform;
+        }
+        else
+        {
+            m_mainCamera.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        // 自分に対してだけ入力を検出して行われる処理をする
+        if (m_photonView.IsMine)
+        {
+            Aim();
+            Shoot();
+            Rotate();
+            Move();
+        }
+    }
+
+    /// <summary>
+    /// Controls camera rotation oriented by mouse position
+    /// </summary>
+    private void Rotate()
+    {
+        transform.Rotate(Vector3.up * sensitivity * Time.deltaTime * Input.GetAxis("Mouse X"));
+
+        m_camRotation.x -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        m_camRotation.x = Mathf.Clamp(m_camRotation.x, minAngle, maxAngle);
+
+        m_cameraTransform.localEulerAngles = m_camRotation;
+    }
+
+    /// <summary>
+    /// 照準を操作する
+    /// 照準にダメージを与えられる対象がいる場合に照準の色を変え、その対象を m_target に保存する
+    /// </summary>
+    void Aim()
+    {
+        Ray ray = m_mainCamera.ScreenPointToRay(m_crosshair.rectTransform.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, m_shootRange, m_shootingLayer))
+        {
+            m_target = hit.collider.GetComponent<Damageable>();
+
+            if (m_target)
+            {
+                m_crosshair.color = m_onTarget;
+            }
+            else
+            {
+                m_crosshair.color = m_noTarget;
+            }
+        }
+        else
+        {
+            m_target = null;
+            m_crosshair.color = m_noTarget;
+        }
+    }
+
+    /// <summary>
+    /// Controls movement with CharacterController
+    /// </summary>
+    private void Move()
+    {
+        if (m_control.isGrounded)
+        {
+            m_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            m_moveDirection = transform.TransformDirection(m_moveDirection);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                m_moveDirection.y = jumpSpeed;
+            }
+        }
+
+        m_moveDirection.y -= gravity * Time.deltaTime;
+        m_control.Move(m_moveDirection * speed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// 敵を撃つ
+    /// </summary>
+    private void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (m_target)
+            {
+                m_target.Damage(PhotonNetwork.LocalPlayer.ActorNumber, 10);
+            }
+        }
+    }
+}
